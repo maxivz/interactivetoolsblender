655cdc1f384479f2d6b1c187fc95c09c102f7551
Maxi <maxivz94@yahoo.com>
2024-08-05 18:13:43 +0200

    Fixed bug with version number reporting

--- utils/user_prefs.py
+++ utils/user_prefs.py
@@ -428,7 +428,7 @@ class AddonPreferences(AddonPreferences):
         row = box.row(align=True)
         row.prop(self, "enable_legacy_tools", toggle=False)
 
-        if float(bpy.app.version_string[:3]) >= 2.82:
+        if bpy.app.version >= (2,82,0):
             row = box.row(align=True)
             row.prop(self, "enable_legacy_origin", toggle=False)
 
