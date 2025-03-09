37bbf89ecd804c32bcca2e7c2d2b4f279d31e0e6
Maxi <maxivz94@yahoo.com>
2025-03-09 07:58:13 +0100

    Added new obj orientation settings to pie menu, added plane prim

--- ui/pie_menus/make_new.py
+++ ui/pie_menus/make_new.py
@@ -1,12 +1,27 @@
 import bpy
 from bpy.types import Menu
 
+NEW_OBJ_ALIGNMENT = "itools_new_obj_alignment"
+
+bpy.types.Scene.itools_new_obj_alignment = bpy.props.EnumProperty(
+    items=[
+        ('WORLD', "World", "First option"),
+        ('CURSOR', "Cursor", "Second option"),
+        ('VIEW', "View", "Third option")
+    ],
+    name="Dropdown Setting",
+    description="Choose an option"
+)
+
 class VIEW3D_MT_PIE_Make_New(Menu):
     bl_label = "New"
 
     def draw(self, context):
         layout = self.layout
         pie = layout.menu_pie()
+
+        #Get orientation Setting
+
 
         # 1 - LEFT
         submenu = pie.column()
@@ -24,25 +39,29 @@ class VIEW3D_MT_PIE_Make_New(Menu):
         row = column.row(align=True)
         row.operator("object.light_add", text="Light", icon="OUTLINER_OB_LIGHT").type = 'POINT'
 
-
-
         # 2 - RIGHT
         submenu = pie.column()
         container = submenu.box()
         column = container.column()
 
+        obj_align = context.scene.itools_new_obj_alignment
+
         row = column.row(align=True)
-        row.operator("mesh.primitive_cube_add", text="Cube", icon="MESH_CUBE")
+        row.operator("mesh.primitive_plane_add", text="Plane", icon="MESH_PLANE").align = obj_align
+
+
+        row = column.row(align=True)
+        row.operator("mesh.primitive_cube_add", text="Cube", icon="MESH_CUBE").align = obj_align
         row.operator("wm.tool_set_by_id", text="", icon="GREASEPENCIL").name = "builtin.primitive_cube_add"
 
 
         row = column.row(align=True)
-        row.operator("mesh.primitive_cylinder_add", text="Cylinder", icon="MESH_CYLINDER")
+        row.operator("mesh.primitive_cylinder_add", text="Cylinder", icon="MESH_CYLINDER").align = obj_align
         row.operator("wm.tool_set_by_id", text="", icon="GREASEPENCIL").name = "builtin.primitive_cylinder_add"
 
 
         row = column.row(align=True)
-        row.operator("mesh.primitive_uv_sphere_add", text="Sphere", icon="MESH_UVSPHERE")
+        row.operator("mesh.primitive_uv_sphere_add", text="Sphere", icon="MESH_UVSPHERE").align = obj_align
         row.operator("wm.tool_set_by_id", text="", icon="GREASEPENCIL").name = "builtin.primitive_uv_sphere_add"
 
 
@@ -50,6 +69,10 @@ class VIEW3D_MT_PIE_Make_New(Menu):
         row.operator("curve.add_bezier_simple", text="Curve", icon="IPO_EASE_IN").mode = "Simple"
         row.emboss = "PULLDOWN_MENU"
         row.operator("curve.add_bezier_simple", text="", icon="GREASEPENCIL").mode = "Draw"
+
+        row = column.row(align=True)
+
+        row.prop(context.scene, NEW_OBJ_ALIGNMENT, text="Align")
 
         
 
@@ -68,4 +91,10 @@ class VIEW3D_MT_PIE_Make_New(Menu):
             text="Collection Instance..." if has_collections else "No Collections to Instance",
             icon='OUTLINER_OB_GROUP_INSTANCE',
         )
+
+        # 4 - TOP
+
+        
+        
+
 
